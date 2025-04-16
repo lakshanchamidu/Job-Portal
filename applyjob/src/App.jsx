c391b4985a04c79b7186190ebd78b57712b6088a
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-04-16 16:45:01 +0530

    Create Navbar

---
+++ applyjob/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../../navbar/src/Navbar/navbar";
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ navbar/src/Navbar/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <nav className="bg-black shadow-md p-6 rounded-b-4xl">
+        <div className="container mx-auto flex justify-between items-center">
+          <a className="text-3xl font-bold text-white tracking-widest pl-[60px]">
+            JobPortal
+          </a>
+          <div className="hidden md:flex space-x-6 gap-3 text-[17px] pr-[60px]">
+            <a
+              href="/home"
+              className="text-white hover:text-blue-500 tracking-widest"
+            >
+              Home
+            </a>
+            <a
+              href="/apply"
+              className="text-white hover:text-blue-500 tracking-widest"
+            >
+              Apply
+            </a>
+            <a
+              href="/profile"
+              className="text-white hover:text-blue-500 tracking-widest"
+            >
+              Profile
+            </a>
+            <a
+              href="/admin"
+              className="text-white hover:text-blue-500 tracking-widest"
+            >
+              Admin
+            </a>
+            <a className="text-white hover:text-blue-500 tracking-widest">
+              LogOut
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default navbar;
