2e3f5c6d1df0aad632f584aafe72c78fb369637d
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-05-13 22:19:42 +0530

    Setup Admin dashboard
    – Setup admin dashboard window.
    – Create vite project and added tailwind css.

---
+++ admin_dashboard/src/AdminDashboard/adminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function adminDashboard() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-black to-gray-800">
+      <div className="text-4xl font-bold p-4 text-white pt-8 tracking-wider">
+        ADMIN DASHBOARD
+      </div>
+    </div>
+  );
+}
+
+export default adminDashboard;
---
+++ admin_dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminDashboard from "./AdminDashboard/adminDashboard";
+
+function App() {
+  return (
+    <div>
+      <AdminDashboard />
+    </div>
+  );
+}
+
+export default App;
